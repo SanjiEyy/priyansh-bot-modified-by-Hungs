0404217161dc90f8aa42b22af4c5f151b17641a3
SanjiEyy <sanjiabc94@gmail.com>
2024-07-10 10:25:51 +0800

    Create -0cnamebot.js

---
+++ Priyansh/commands/-0cnamebot.js
@@ -0,0 +1,33 @@
+module.exports.config = {
+    name: "cnamebot",
+    version: "1.0.4",
+    hasPermssion: 0,
+    creditss: "datoccho",
+    description: "Automatically prevent change bot nickname",
+    commandCategory: "system",
+    usages: "",
+    cooldowns: 5
+};
+
+module.exports.handleEvent = async function ({ api, event }) {
+    const { threadID } = event;
+    let { nicknames } = await api.getThreadInfo(event.threadID);
+    const nameBot = nicknames[api.getCurrentUserID()];
+
+    if (nameBot !== `》 ${global.config.PREFIX} 《 ❃ ➠${!global.config.BOTNAME ? " " : global.config.BOTNAME}`) {
+        api.changeNickname(`》 ${global.config.PREFIX} 《 ❃ ➠${!global.config.BOTNAME ? "Made by CatalizCS and SpermLord" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+    }
+};
+
+module.exports.run = async function ({ api, event, Threads }) {
+    let data = (await Threads.getData(event.threadID)).data || {};
+
+    if (typeof data["cnamebot"] == "undefined" || data["cnamebot"] == false) {
+        data["cnamebot"] = true;
+    } else {
+        data["cnamebot"] = false;
+    }
+    
+    await Threads.setData(event.threadID, { data });
+    global.data.threadData.set(parseInt(event.threadID), data);
+};
